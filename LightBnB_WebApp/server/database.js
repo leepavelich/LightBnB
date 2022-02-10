7f49ce55b0aa798b23071ed0b0258ab0f86fa1af
Lee Pavelich <leepavelich@gmail.com>
2022-02-10 22:09:30 +0000

    Update functions to use ES6 arrow syntax

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -17,7 +17,7 @@ const users = require('./json/users.json');
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
+const getUserWithEmail = (email) => {
   return pool
     .query('SELECT * FROM users WHERE email = $1', [email])
     .then((result) => result.rows[0])
@@ -30,7 +30,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
+const getUserWithId = (id) => {
   return pool
     .query('SELECT * FROM users WHERE id = $1', [id])
     .then((result) => result.rows[0])
@@ -44,7 +44,7 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
+const addUser = (user) => {
   return pool
     .query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;', [user.name, user.email, user.password])
     .then((result) => result.rows[0])
